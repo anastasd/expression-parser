d9089f73fadfcb661c1743428c778caefe991606
Anastas Dolushanov <anastas.dolushanov@remote-tech.co.uk>
2024-10-18 18:54:43 +0300

    2024-10-18

---
+++ tests/test.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once __DIR__ . '/../src/AnastasD/ExpressionParser/Settings.php';
+include_once __DIR__ . '/../src/AnastasD/ExpressionParser/Parser.php';
+
+include_once __DIR__ . '/../src/AnastasD/ExpressionParser/Mappers/Mapper.php';
+include_once __DIR__ . '/../src/AnastasD/ExpressionParser/Mappers/MapperPHP.php';
+include_once __DIR__ . '/../src/AnastasD/ExpressionParser/Evaluators/Evaluator.php';
+include_once __DIR__ . '/../src/AnastasD/ExpressionParser/Evaluators/EvaluatorPHP.php';
+include_once __DIR__ . '/../src/AnastasD/ExpressionParser/Evaluators/EvaluatorHelpers/EvaluatorHelper.php';
+include_once __DIR__ . '/../src/AnastasD/ExpressionParser/Evaluators/EvaluatorHelpers/EvaluatorHelperPHP.php';
+include_once __DIR__ . '/../src/AnastasD/ExpressionParser/Compilers/Compiler.php';
+include_once __DIR__ . '/../src/AnastasD/ExpressionParser/Compilers/CompilerPHP.php';
+include_once __DIR__ . '/../src/AnastasD/ExpressionParser/Compilers/CompilerHelpers/CompilerHelper.php';
+include_once __DIR__ . '/../src/AnastasD/ExpressionParser/Compilers/CompilerHelpers/CompilerHelperPHP.php';
+
+include_once __DIR__ . '/../src/AnastasD/ExpressionParser/Validators/Validator.php';
+
+
+use AnastasD\ExpressionParser\Settings;
+use AnastasD\ExpressionParser\Parser;
+
+
+class TestRegex
+{
+    public function testVariableRegex()
+    {
+        $settings = new Settings(Settings::PHP);
+
+        $parser = new Parser($settings);
+
+        $input = '$a+1';
+
+        $output = $parser->parse($input, ['$a'])
+            ->prepare()
+            ->compile(["compress"]);
+
+            echo $output;
+    }
+}
+
+$t = new TestRegex();
+$t->testVariableRegex();
